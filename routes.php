462f18fd733b8f9ab57991116d03093a913c022e
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-23 15:33:22 +0100

    add routes

--- routes.php
+++ routes.php
@@ -3,3 +3,15 @@
 
 $router->get("/", "controllers/index.php");
 $router->get("/about", "controllers/about.php");
+$router->get("/contact", "controllers/contact.php");
+
+// blogs
+$router->get("/blogs", "controllers/blogs/index.php");
+$router->get("/blogs/create", "controllers/blogs/create.php");
+$router->post("/blogs", "controllers/blogs/store.php");
+
+//blog
+$router->get("/blog", "controllers/blogs/show.php");
+$router->patch("/blog", "controllers/blogs/update.php");
+$router->delete("/blog", "controllers/blogs/destroy.php");
+$router->get("/blog/edit", "controllers/blogs/edit.php");
