7f054c645bdc68047d5b29c971c60a7e15511006
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-26 10:02:13 +0100

    parse uri to handle parameters

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $router = new Router();
 
 require("routes.php");
 
-$uri = $_SERVER["REQUEST_URI"];
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 $method = $_POST["_method"] ?? $_SERVER["REQUEST_METHOD"];
 
 $router->route($uri, $method);
