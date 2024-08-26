c7d634928bbfdae4afa7bbc55c70cf20b0cf61a7
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-26 13:39:03 +0100

    update controller to use new App class container for DB

--- controllers/blogs/show.php
+++ controllers/blogs/show.php
@@ -1,11 +1,11 @@
 <?php
 
+use core\App;
 use core\Database;
 
 $heading = "Post";
 
-$config = require("config.php");
-$db = new Database($config["database"]);
+$db = App::resolve(Database::class);
 
 $post = $db->query("SELECT * FROM posts WHERE post_id = :id",[
   "id" => $_GET["id"]
