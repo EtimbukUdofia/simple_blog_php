8e118ff690a070155903b9a6082f7a1005823647
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-27 21:57:30 +0100

    add ValidationException to handle exceptions from bad inputs start sessions

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 use core\Router;
+use core\Session;
+use core\ValidationException;
 
 require("core/functions.php");
 
@@ -8,6 +10,8 @@ spl_autoload_register(function($classname){
   $classname = str_replace("\\", DIRECTORY_SEPARATOR, $classname);
   require $classname . ".php";
 });
+
+session_start();
 
 require("bootstrap.php");
 
@@ -18,4 +22,13 @@ require("routes.php");
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 $method = $_POST["_method"] ?? $_SERVER["REQUEST_METHOD"];
 
-$router->route($uri, $method);
+try{
+  $router->route($uri, $method);
+} catch(ValidationException $exception){
+  Session::flash("errors", $exception->errors);
+  Session::flash("old", $exception->old);
+
+  return redirect($router->previuosUrl());
+};
+
+Session::unFlash();
