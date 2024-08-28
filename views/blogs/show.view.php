283eebed171a90d47aba9bee4084c02c6ca7075f
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-28 09:39:08 +0100

    update view

--- views/blogs/show.view.php
+++ views/blogs/show.view.php
@@ -12,7 +12,7 @@
     </p>
 
     <footer class="mt-6">
-      <!-- <a href="/note/edit?id=<?= $note["id"] ?>" class="ounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded-lg border border-current">Edit</a> -->
+      <!-- <a href="/note/edit?id=<?//= $note["id"] ?>" class="ounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded-lg border border-current">Edit</a> -->
     </footer>
   </div>
 </main>
@@ -20,6 +20,6 @@
 
 <!-- <form class="mt-6" method="POST">
   <input type="hidden" name="_method" value="DELETE">
-  <input type="hidden" value="<?= $note["id"] ?>" name="id">
+  <input type="hidden" value="<?//= $note["id"] ?>" name="id">
   <button class="text=-sm text-red-500">Delete</button>
 </form> -->
