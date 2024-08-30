a6d98ad7085c406a1ccbab85358fda248936a016
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-08-30 11:36:37 +0100

    update Sessions class to handle destruction of current user session

--- core/Session.php
+++ core/Session.php
@@ -26,4 +26,15 @@ class Session{
   public static function unFlash(){
     unset($_SESSION["_flash"]);
   }
+
+  public static function flush(){
+    $_SESSION = [];
+  }
+
+  public static function destroy(){
+    static::flush();
+    session_destroy();
+    $params = session_get_cookie_params();
+    setcookie("PHPSESSID", "", time() - 3600, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+  }
 }
